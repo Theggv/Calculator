08be33d8bb88a7d5748b9159ebe40a5d6522eaca
Твой <vasvasal@gmail.com>
2018-04-13 03:10:03 +0500

    Сделал готовый класс данных

---
+++ WpfApp3/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    class Calculator
+    {
+        public static Fraction ChangeDomDen(Fraction a) //Change places of devider an denominator
+        {
+            int c = a.Divider;
+            a.Numerator = a.Divider;
+            a.Divider = c;
+            return a;
+        }
+    }
+}
--- WpfApp3/Fraction.cs
+++ WpfApp3/Fraction.cs
@@ -3,13 +3,119 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static WpfApp3.Calculator;
 
 namespace WpfApp3
 {
     class Fraction
     {
-        public static long Divider;         //Числитель
-        public static long Denominator;     //Знаменатель
-        public static long Total;           //Целая часть
+        public static int numerator;              //Числитель
+        public static int divider;          //Знаменатель
+
+        public int Numerator                      //Условие числителя
+        {
+            get { return numerator; }
+            set
+            {
+                if (numerator < 0 && divider < 0)
+                {
+                    numerator = Math.Abs(numerator);
+                    divider = Math.Abs(divider);
+                }
+                else numerator = value;
+            }
+        }
+
+        public int Divider                  //Условие знаменателя
+        {
+            get { return divider; }
+            set
+            {
+                if (divider == 0)
+                    throw new Exception("Знаменатель не может быть равен нулю!");
+                else if (numerator < 0 && divider < 0)
+                {
+                    numerator = Math.Abs(numerator);
+                    divider = Math.Abs(divider);
+                }
+                else divider = value;
+            }
+        }
+
+        public Fraction(int up, int down)       //Конструктор с параметрами
+        {
+            numerator = up;
+            divider = down;
+        }
+
+        public Fraction()                       //Конструктор без параметров
+        {
+            numerator = 0;
+            divider = 1;
+        }
+
+        public static Fraction operator *(Fraction a, int value) //Оператор умножения дроби на число девосторонний
+        {
+            Fraction result = a;
+
+            result.Divider *= value;
+            result.Numerator *= value;
+
+            return result;
+        }
+
+        public static Fraction operator *(int value, Fraction a) //Оператор умножения дроби на число правосторонний
+        {
+            Fraction result = a;
+
+            result.Divider *= value;
+            result.Numerator *= value;
+
+            return result;
+        }
+
+        public static Fraction operator *(Fraction a, Fraction b) //Оператор умножения дроби на дробь
+        {
+            Fraction result = new Fraction();
+
+            result.Divider = a.Divider * b.Divider;
+            result.Numerator = a.Numerator * b.Numerator;
+
+            return result;
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b) //Оператор сложения
+        {
+            Fraction result = new Fraction();
+
+            result.Divider = a.Divider * b.Divider;
+            a *= b.Divider;
+            b *= a.Divider;
+            result.Numerator = a.Numerator + b.Numerator;
+            return result;
+        }
+
+        public static Fraction operator -(Fraction a, Fraction b) //Оператор вычитания
+        {
+            Fraction result = new Fraction();
+
+            result.Divider = a.Divider * b.Divider;
+            a *= b.Divider;
+            b *= a.Divider;
+            result.Numerator = a.Numerator - b.Numerator;
+            return result;
+        }
+
+        public static Fraction operator /(Fraction a, Fraction b) //Оператор умножения
+        {
+            Fraction result = new Fraction();
+
+            b = ChangeDomDen(b);
+
+            result.Divider = a.Divider * b.Divider;
+            result.Numerator = a.Numerator * b.Numerator;
+
+            return result;
+        }
     }
 }
